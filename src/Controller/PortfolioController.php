2881d35168ccf0038d74c730a0a134ed1b30437f
Ludo <85347571+Ludo-Kaliom@users.noreply.github.com>
2022-05-16 11:45:27 +0200

    ajout fixtures connexion Admin

--- src/Controller/PortfolioController.php
+++ src/Controller/PortfolioController.php
@@ -12,13 +12,11 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
-use Symfony\Component\Mailer\MailerInterface;
-use Symfony\Component\Mime\Email;
 
 class PortfolioController extends AbstractController
 {
     #[Route('/', name: 'app_portfolio')]
-    public function index(ProjectsRepository $projectsRepository, SkillsRepository $skillsRepository, PresentationRepository $presentationRepository, EntityManagerInterface $em, Request $request, MailerInterface $mailer): Response
+    public function index(ProjectsRepository $projectsRepository, SkillsRepository $skillsRepository, PresentationRepository $presentationRepository, EntityManagerInterface $em, Request $request): Response
     {
         $contact = new Contact();
         $form = $this->createForm(ContactType::class, $contact);
@@ -28,18 +26,6 @@ class PortfolioController extends AbstractController
                 $em->persist($contact);
                 $em->flush();
                 $this->addFlash('success', 'Votre message a bien été envoyé, je vais vous recontacter très rapidement');
-
-                $email = (new Email())
-                ->from('Kaliom.Geefker@gmail.com')
-                ->to('L.P.Kaffin@gmail.com')
-                //->cc('cc@example.com')
-                //->bcc('bcc@example.com')
-                //->replyTo('fabien@example.com')
-                //->priority(Email::PRIORITY_HIGH)
-                ->subject('Time for Symfony Mailer!')
-                ->text('Sending emails is fun again!')
-                ->html('<p>See Twig integration for better HTML integration!</p>');
-                $mailer->send($email);
             }
 
         return $this->render('portfolio/index.html.twig', [
--- src/DataFixtures/UsersFixtures.php
+++ src/DataFixtures/UsersFixtures.php
@@ -2,16 +2,25 @@
 
 namespace App\DataFixtures;
 
-use Doctrine\Bundle\FixturesBundle\Fixture;
+use App\Entity\User;
 use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class UsersFixtures extends Fixture
 {
+
+    public function __construct(
+        private UserPasswordHasherInterface $hasher
+    ) {}
+    
     public function load(ObjectManager $manager): void
     {
-        // $product = new Product();
-        // $manager->persist($product);
-
+        $admin = new User();
+        $admin->setEmail('Admin@admin.com');
+        $admin->setPassword($this->hasher->hashPassword($admin, 'Admin@admin.com'));
+        $admin->setRoles(['ROLE_ADMIN']);
+        $manager->persist($admin);
         $manager->flush();
     }
 }
